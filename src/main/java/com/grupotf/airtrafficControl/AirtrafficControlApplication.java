3de6b086fd391d5b1ecd6b35e7f71b3c408df5c6
LucasTBandeira <l.treviso@edu.pucrs.br>
2023-06-12 18:00:47 -0300

    Spring Project

---
+++ src/main/java/com/grupotf/airtrafficControl/AirtrafficControlApplication.java
@@ -0,0 +1,13 @@
+package com.grupotf.airtrafficControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirtrafficControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirtrafficControlApplication.class, args);
+	}
+
+}
